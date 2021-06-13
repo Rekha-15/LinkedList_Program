a0c54d0aa2f1c0a03aae1d891431178e121684f8
Rekha-15 <rekhapatil.1509@gmail.com>
2021-06-13 12:21:23 -0400

    add : LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -38,6 +38,33 @@ public class LinkedList {
 		}	
 			}
 	
+	
+	
+	/**
+	 * method add is used to add the node at index
+	 * @param index
+	 * @param data
+	 */
+	public void add(int index,int data) {
+		Node newNode = new Node();
+		newNode.data=data;
+		newNode.next=null;
+		
+		if(index == 0) {
+			newNode.next=head;
+			head=newNode;
+		}else {
+			Node temp = head;
+			for(int i=0;i<(index-1);i++) {
+				temp=temp.next;
+			}
+			newNode.next=temp.next;
+			temp.next=newNode;
+		}
+		
+		
+	}
+	
 	/**
 	 * void show method
 	 * prints the node
